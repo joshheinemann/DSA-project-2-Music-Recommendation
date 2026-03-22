1b0c74cf2d880d9b16fd1fca000a039f228a0f5a
joshheinemann <joshuaheinemann@ufl.edu>
2026-03-22 12:03:41 -0400

    Fix Song class

--- src/Song.cpp
+++ src/Song.cpp
@@ -1,10 +1,10 @@
 #include "Song.h"
 
-Song::Song(std::string id, std::string name, std::string artist, std::string album,
+Song::Song(std::string id, std::string name, std::vector<std::string> artists, std::string album,
            double danceability, double energy, double valence, double tempo,
            double acousticness, double instrumentalness, double loudness,
            double speechiness, double liveness, int key, int mode, int duration_ms)
-    : id(id), name(name), artist(artist), album(album),
+    : id(id), name(name), artists(artists), album(album),
       danceability(danceability), energy(energy), valence(valence), tempo(tempo),
       acousticness(acousticness), instrumentalness(instrumentalness),
       loudness(loudness), speechiness(speechiness), liveness(liveness),
@@ -14,6 +14,6 @@ std::string Song::getName() const {
     return name;
 }
 
-std::string Song::getArtist() const {
-    return artist;
+std::vector<std::string> Song::getArtists() const {
+    return artists;
 }
