966ad1f40f9b10403f6a8924622b6334c9971118
joshheinemann <joshuaheinemann@ufl.edu>
2026-03-22 11:04:17 -0400

    Implement Song class cpp file

---
+++ src/Song.cpp
@@ -0,0 +1,19 @@
+#include "Song.h"
+
+Song::Song(std::string id, std::string name, std::string artist, std::string album,
+           double danceability, double energy, double valence, double tempo,
+           double acousticness, double instrumentalness, double loudness,
+           double speechiness, double liveness, int key, int mode, int duration_ms)
+    : id(id), name(name), artist(artist), album(album),
+      danceability(danceability), energy(energy), valence(valence), tempo(tempo),
+      acousticness(acousticness), instrumentalness(instrumentalness),
+      loudness(loudness), speechiness(speechiness), liveness(liveness),
+      key(key), mode(mode), duration_ms(duration_ms) {}
+
+std::string Song::getName() const {
+    return name;
+}
+
+std::string Song::getArtist() const {
+    return artist;
+}
